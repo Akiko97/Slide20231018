271e0ba71be28dc19935baa2c4a896fb66b96281
Gabriel Zhong <37476779+Akiko97@users.noreply.github.com>
2023-10-18 02:20:06 +0900

    update

--- src/lib/pages/page5.svelte
+++ src/lib/pages/page5.svelte
@@ -10,12 +10,19 @@
   import { Instructions } from '$lib/pages/Anime/src/instructions.js'
   import { onMount } from 'svelte'
   registers.update(() => [
-    { name: 'YMM0', size: 8, values: [8, 1, 3, 5, 7, 9, 2, 4] },
-    { name: 'YMM1', size: 8, values: [1, 3, 5, 7, 9, 2, 4, 6] },
-    { name: 'YMM2', size: 8, values: [0, 0, 0, 0, 0, 0, 0, 0] },
-    { name: 'YMM3', size: 8, values: [0, 0, 0, 0, 0, 0, 0, 0] },
-    { name: 'YMM4', size: 8, values: [0, 0, 0, 0, 0, 0, 0, 0] },
-    { name: 'YMM5', size: 8, values: [0, 0, 0, 0, 0, 0, 0, 0] },
+    { name: 'YMM0', size: 4, values: [3, 2, 1, 0] },
+    { name: 'YMM1', size: 4, values: [7, 6, 5, 4] },
+    { name: 'YMM2', size: 4, values: [11, 10, 9, 8] },
+    { name: 'YMM3', size: 4, values: [15, 14, 13, 12] },
+    { name: 'XMM0', size: 2, values: [0, 0] },
+    { name: 'YMM4', size: 4, values: [0, 0, 0, 0] },
+    { name: 'YMM5', size: 4, values: [0, 0, 0, 0] },
+    { name: 'YMM6', size: 4, values: [0, 0, 0, 0] },
+    { name: 'YMM7', size: 4, values: [0, 0, 0, 0] },
+    { name: 'YMM8', size: 4, values: [0, 0, 0, 0] },
+    { name: 'YMM9', size: 4, values: [0, 0, 0, 0] },
+    { name: 'YMM10', size: 4, values: [0, 0, 0, 0] },
+    { name: 'YMM11', size: 4, values: [0, 0, 0, 0] },
   ])
   let i:any
   onMount(() => {
@@ -36,14 +43,62 @@
     removeHighlight:()=>void
   }
   code.update(() => {
-    return 'vperm2f128 ymm0, ymm0, ymm0, 0x01\n' +
-      'movaps ymm2, ymm0\n' +
-      'movaps ymm3, ymm1\n' +
-      'vshufps ymm4, ymm2, ymm3, 0x4E\n' +
-      'vperm2f128 ymm5, ymm2, ymm3, 0x20\n' +
-      'vpermq ymm5, ymm5, 0x4E\n' +
-      'vxorps ymm0, ymm0, ymm0\n' +
-      'vxorps ymm0, ymm1, ymm2'
+    return '; Calculate 1st row of result (YMM8)\n' +
+      'vextractf128 xmm0, ymm0, 1\n' +
+      'vbroadcastsd ymm5, xmm0\n' +
+      'shufpd xmm0, xmm0, 1\n' +
+      'vbroadcastsd ymm4, xmm0\n' +
+      'vextractf128 xmm0, ymm0, 0\n' +
+      'vbroadcastsd ymm7, xmm0\n' +
+      'shufpd xmm0, xmm0, 1\n' +
+      'vbroadcastsd ymm6, xmm0\n' +
+      'vmulpd ymm7, ymm3, ymm7\n' +
+      'vfmadd213pd ymm6, ymm2, ymm7\n' +
+      'vfmadd213pd ymm5, ymm1, ymm6\n' +
+      'vfmadd213pd ymm4, ymm0, ymm5\n' +
+      'vmovapd ymm8, ymm4\n' +
+      '; Calculate 2nd row of result (YMM9)\n' +
+      'vextractf128 xmm0, ymm1, 1\n' +
+      'vbroadcastsd ymm5, xmm0\n' +
+      'shufpd xmm0, xmm0, 1\n' +
+      'vbroadcastsd ymm4, xmm0\n' +
+      'vextractf128 xmm0, ymm1, 0\n' +
+      'vbroadcastsd ymm7, xmm0\n' +
+      'shufpd xmm0, xmm0, 1\n' +
+      'vbroadcastsd ymm6, xmm0\n' +
+      'vmulpd ymm7, ymm3, ymm7\n' +
+      'vfmadd213pd ymm6, ymm2, ymm7\n' +
+      'vfmadd213pd ymm5, ymm1, ymm6\n' +
+      'vfmadd213pd ymm4, ymm0, ymm5\n' +
+      'vmovapd ymm9, ymm4\n' +
+      '; Calculate 3rd row of result (YMM10)\n' +
+      'vextractf128 xmm0, ymm2, 1\n' +
+      'vbroadcastsd ymm5, xmm0\n' +
+      'shufpd xmm0, xmm0, 1\n' +
+      'vbroadcastsd ymm4, xmm0\n' +
+      'vextractf128 xmm0, ymm2, 0\n' +
+      'vbroadcastsd ymm7, xmm0\n' +
+      'shufpd xmm0, xmm0, 1\n' +
+      'vbroadcastsd ymm6, xmm0\n' +
+      'vmulpd ymm7, ymm3, ymm7\n' +
+      'vfmadd213pd ymm6, ymm2, ymm7\n' +
+      'vfmadd213pd ymm5, ymm1, ymm6\n' +
+      'vfmadd213pd ymm4, ymm0, ymm5\n' +
+      'vmovapd ymm10, ymm4\n' +
+      '; Calculate 4th row of result (YMM11)\n' +
+      'vextractf128 xmm0, ymm3, 1\n' +
+      'vbroadcastsd ymm5, xmm0\n' +
+      'shufpd xmm0, xmm0, 1\n' +
+      'vbroadcastsd ymm4, xmm0\n' +
+      'vextractf128 xmm0, ymm3, 0\n' +
+      'vbroadcastsd ymm7, xmm0\n' +
+      'shufpd xmm0, xmm0, 1\n' +
+      'vbroadcastsd ymm6, xmm0\n' +
+      'vmulpd ymm7, ymm3, ymm7\n' +
+      'vfmadd213pd ymm6, ymm2, ymm7\n' +
+      'vfmadd213pd ymm5, ymm1, ymm6\n' +
+      'vfmadd213pd ymm4, ymm0, ymm5\n' +
+      'vmovapd ymm11, ymm4'
   })
   const executeInstruction = (instructionStr:string) => {
     const tokens = instructionStr.split(/[\s,]+/)
@@ -65,12 +120,19 @@
     if (step > $code.split('\n').length) {
       step = 1
       registers.update(() => [
-        { name: 'YMM0', size: 8, values: [8, 1, 3, 5, 7, 9, 2, 4] },
-        { name: 'YMM1', size: 8, values: [1, 3, 5, 7, 9, 2, 4, 6] },
-        { name: 'YMM2', size: 8, values: [0, 0, 0, 0, 0, 0, 0, 0] },
-        { name: 'YMM3', size: 8, values: [0, 0, 0, 0, 0, 0, 0, 0] },
-        { name: 'YMM4', size: 8, values: [0, 0, 0, 0, 0, 0, 0, 0] },
-        { name: 'YMM5', size: 8, values: [0, 0, 0, 0, 0, 0, 0, 0] },
+        { name: 'YMM0', size: 4, values: [3, 2, 1, 0] },
+        { name: 'YMM1', size: 4, values: [7, 6, 5, 4] },
+        { name: 'YMM2', size: 4, values: [11, 10, 9, 8] },
+        { name: 'YMM3', size: 4, values: [15, 14, 13, 12] },
+        { name: 'XMM0', size: 2, values: [0, 0] },
+        { name: 'YMM4', size: 4, values: [0, 0, 0, 0] },
+        { name: 'YMM5', size: 4, values: [0, 0, 0, 0] },
+        { name: 'YMM6', size: 4, values: [0, 0, 0, 0] },
+        { name: 'YMM7', size: 4, values: [0, 0, 0, 0] },
+        { name: 'YMM8', size: 4, values: [0, 0, 0, 0] },
+        { name: 'YMM9', size: 4, values: [0, 0, 0, 0] },
+        { name: 'YMM10', size: 4, values: [0, 0, 0, 0] },
+        { name: 'YMM11', size: 4, values: [0, 0, 0, 0] },
       ])
     }
     else {
@@ -96,16 +158,20 @@
     <div data-id="t6" class="text-4xl mb-12">
       Implemented Features
     </div>
-    <div class="text-3xl">
+    <div class="text-xl">
       Instruction Visualization example: Calculating matrix multiplication.
     </div>
     <div class="w-[100%] h-[500px] flex text-xl">
-      <div class="w-[40%] text-left p-[5px]">
+      <div class="w-[50%] text-left p-[5px]">
         <Editor bind:this={editorRef} />
       </div>
-      <div class="w-[60%] p-[5px]">
-        <Visualization bind:this={visualizationRef} />
-        <Button click={run} text="Run" />
+      <div class="w-[50%] p-[5px]">
+        <div class="vc">
+          <Visualization bind:this={visualizationRef} />
+        </div>
+        <div class="mt-[20px]">
+          <Button click={run} text="Step" />
+        </div>
       </div>
     </div>
     <Notes>
@@ -133,3 +199,23 @@
     </Notes>
   </Page>
 </Page>
+
+<style>
+  .vc {
+    height: 450px;
+    overflow-y: scroll;
+    overflow-x: hidden;
+  }
+  .vc::-webkit-scrollbar {
+    width: 12px;
+  }
+  .vc::-webkit-scrollbar-track {
+    background-color: #777;
+  }
+  .vc::-webkit-scrollbar-thumb {
+    background-color: #666;
+  }
+  .vc::-webkit-scrollbar-thumb:hover {
+    background-color: #555;
+  }
+</style>
